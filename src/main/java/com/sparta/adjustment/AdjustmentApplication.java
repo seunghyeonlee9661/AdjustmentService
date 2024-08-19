85f19e04b24910f09e623b18444151fe1e062d0e
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-19 12:10:33 +0900

    Initial commit

---
+++ src/main/java/com/sparta/adjustment/AdjustmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.adjustment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjustmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdjustmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/adjustment/AdjustmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.adjustment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdjustmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
