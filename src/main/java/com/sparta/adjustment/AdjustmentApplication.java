a8a49f2ac5b1782aea5d2d9ac2996859a4aa81b6
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-20 19:05:45 +0900

    코드 테스트

--- src/main/java/com/sparta/adjustment/AdjustmentApplication.java
+++ src/main/java/com/sparta/adjustment/AdjustmentApplication.java
@@ -1,23 +1,26 @@
 package com.sparta.adjustment;
-
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.TimeZone;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sparta.adjustment") // 필요한 패키지 경로 지정
 public class AdjustmentApplication {
-
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdjustmentApplication.class, args);
 	}
+}
 
+@RestController
+@RequestMapping("/api")
+class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
