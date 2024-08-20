c37927f282e00f395cce0abcbd16fb2602b99816
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-20 21:32:58 +0900

    겹치는거 처리 테스트

--- src/main/java/com/sparta/adjustment/AdjustmentApplication.java
+++ src/main/java/com/sparta/adjustment/AdjustmentApplication.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class AdjustmentApplication {
 
