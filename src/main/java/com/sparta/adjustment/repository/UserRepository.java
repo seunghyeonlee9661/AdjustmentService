c802c69c01e474ed761e0db59993406da1efd48d
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-20 21:38:11 +0900

    설정 몇가지 넣어서 재시도

--- src/main/java/com/sparta/adjustment/repository/UserRepository.java
+++ src/main/java/com/sparta/adjustment/repository/UserRepository.java
@@ -1,14 +1,14 @@
-//package com.sparta.adjustment.repository;
-//import com.sparta.adjustment.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    Optional<User> findByUsername(String email);
-//    Optional<User> findByKakaoId(Long kakaoId);
-//    Boolean existsByNickname(String nickname);
-//    Boolean existsByEmail(String email);
-//}
+package com.sparta.adjustment.repository;
+import com.sparta.adjustment.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String email);
+    Optional<User> findByKakaoId(Long kakaoId);
+    Boolean existsByNickname(String nickname);
+    Boolean existsByEmail(String email);
+}
