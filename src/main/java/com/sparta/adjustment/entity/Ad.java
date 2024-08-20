0069acd64f7ba7bf290d8894701a9cf0b981723e
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-20 18:33:12 +0900

    Entity 저장 확인

--- src/main/java/com/sparta/adjustment/entity/Ad.java
+++ src/main/java/com/sparta/adjustment/entity/Ad.java
@@ -1,32 +1,32 @@
-//package com.sparta.adjustment.entity;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//
-//import java.util.List;
-//
-//@Entity
-//@Getter
-//@Table(name = "Ad")
-//public class Ad {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private User user;
-//
-//    @Column(nullable = false)
-//    private String url;
-//
-//    @Column(nullable = false)
-//    private String title;
-//
-//    @Column(nullable = false)
-//    private Long viewCount;
-//
-//    @OneToMany(mappedBy = "ad")
-//    private List<AdList> adLists;
-//
-//    // Getters and Setters
-//}
+package com.sparta.adjustment.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "Ad")
+public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Long viewCount;
+
+    @OneToMany(mappedBy = "ad")
+    private List<AdList> adLists;
+
+    // Getters and Setters
+}
--- src/main/java/com/sparta/adjustment/entity/Role.java
+++ src/main/java/com/sparta/adjustment/entity/Role.java
@@ -1,6 +1,6 @@
-//package com.sparta.adjustment.entity;
-//
-//public enum Role {
-//    ROLE_USER,
-//    ROLE_UPLODER
-//}
+package com.sparta.adjustment.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_UPLODER
+}
