6966a980f1f9ea221cf3e75895b169e9e5be31ea
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-21 22:45:09 +0900

    bootjar 적용

--- user-service/src/main/java/com/sparta/user/UserServiceApplication.java
+++ user-service/src/main/java/com/sparta/user/UserServiceApplication.java
@@ -1,5 +1,4 @@
 package com.sparta.user;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,11 +6,6 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 public class UserServiceApplication {
-
-    @PostConstruct
-    public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);   }
