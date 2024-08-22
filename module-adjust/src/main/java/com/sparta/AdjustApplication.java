55849ea94f5e525f110a295fe74ce5b140712c0f
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-22 21:48:00 +0900

    다시 빌드 방식으로 수정

--- module-adjust/src/main/java/com/sparta/AdjustApplication.java
+++ module-adjust/src/main/java/com/sparta/AdjustApplication.java
@@ -1,26 +1,9 @@
 package com.sparta;
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdjustApplication {
-    // 의존성 확인을 위한 코드 - 시작
-    private final TestBean testBean;
-
-    @Autowired
-    public AdjustApplication(TestBean testBean) {
-        this.testBean = testBean;
-    }
-
-    @PostConstruct
-    public void dependencyTest() {
-        testBean.dependencyTest();
-    }
-    // 의존성 확인을 위한 코드 - 끝
-
     public static void main(String[] args) {
         SpringApplication.run(AdjustApplication.class, args);
     }
