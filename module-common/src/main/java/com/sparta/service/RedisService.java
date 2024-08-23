89406e11e6bcd149f4ad455d58091876ab0c8953
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-23 22:39:50 +0900

    기능 추가

--- module-common/src/main/java/com/sparta/service/RedisService.java
+++ module-common/src/main/java/com/sparta/service/RedisService.java
@@ -5,7 +5,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
-
 /*
 작성자 : 이승현
 * Redis Server에 값을 저장, 조회, 삭제하는 기능
@@ -22,8 +21,8 @@ public class RedisService {
     public static final String VIEW_LIMIT_PREFIX = "viewCount:";    // 24시간 조회수 제한을 위한 상수
     public static final long VIEW_LIMIT_DURATION = 24 * 60 * 60 * 1000; // 24시간을 밀리초로 표현
 
-    public static final String RESET_CODE_PREFIX = "resetCode:"; // Redis에 저장될 비밀번호 재설정 코드의 접두사
-    public static final long RESET_CODE_DURATION = 5 * 60 * 1000; // 5분
+    public static final String UPLOADER_REQUEST_PREFIX = "resetCode:"; // Redis에 저장될 비밀번호 재설정 코드의 접두사
+    public static final long UPLOADER_REQUEST_DURATION = 5 * 60 * 1000; // 5분
 
     public static final String NAVER_SHOPPING_API_PREFIX = "resetCode:"; // Redis에 저장될 비밀번호 재설정 코드의 접두사
     public static final long NAVER_SHOPPING_API_DURATION = 60 * 60 * 1000; // 5분
