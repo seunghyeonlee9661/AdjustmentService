66207fe3876ab34c00d170a16c8d26913e11a380
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-24 12:11:37 +0900

    Video 컨트롤러 추가

--- module-video/src/main/java/com/sparta/controller/VideoController.java
+++ module-video/src/main/java/com/sparta/controller/VideoController.java
@@ -1,14 +1,31 @@
 package com.sparta.controller;
+import com.sparta.dto.VideoDetailResponseDTO;
+import com.sparta.dto.VideoListResponseDTO;
+import com.sparta.security.UserDetailsImpl;
+import com.sparta.service.VideoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("")
-class VideoController {
-    @GetMapping("/test")
-    public String hello() {
-        return "Hello, this is Video Controller";
+@RequestMapping("/api/videos")
+public class VideoController {
+
+    private final VideoService videoService;
+
+    /* 비디오 목록 페이지 불러오기 */
+    @GetMapping("")
+    public ResponseEntity<Page<VideoListResponseDTO>> findVideoList(@RequestParam(value = "page", defaultValue = "0") int page){
+        return videoService.getVideoList(page);
+    }
+
+    /* 비디오 재생 */
+    @GetMapping("/{id}")
+    public ResponseEntity<VideoDetailResponseDTO> playVideo(@PathVariable("id") long id, @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request){
+        return videoService.playVideo(id,userDetails,request);
     }
 }
