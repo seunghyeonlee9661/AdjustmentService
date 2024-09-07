decacb5039f0b803ea286e90e335cc8c879c7cf8
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-07 10:55:58 +0900

    유레카 서버 구동 테스트

--- module-adjust/src/main/java/com/sparta/AdjustApplication.java
+++ module-adjust/src/main/java/com/sparta/AdjustApplication.java
@@ -2,6 +2,7 @@ package com.sparta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.sparta", "com.common"})
 @EnableJpaRepositories(basePackages = {"com.common.repository", "com.sparta.repository"})
 @EnableJpaAuditing
+@EnableDiscoveryClient // 유레카 등록
 public class AdjustApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdjustApplication.class, args);
