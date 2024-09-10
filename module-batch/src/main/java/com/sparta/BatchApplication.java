0fda118c055ad4aeef4c93b72437a556be9cb854
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-10 21:37:06 +0900

    유레카 의존성 정리 및 설정 추가

--- module-batch/src/main/java/com/sparta/BatchApplication.java
+++ module-batch/src/main/java/com/sparta/BatchApplication.java
@@ -1,11 +1,20 @@
 package com.sparta;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling // 추가
 public class BatchApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
--- module-eureka/src/main/java/com/sparta/EurekaServerApplication.java
+++ module-eureka/src/main/java/com/sparta/EurekaServerApplication.java
@@ -1,12 +1,21 @@
 package com.sparta;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
