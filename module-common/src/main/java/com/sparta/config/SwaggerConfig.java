ca749094623cfab63f0611a5a8565e739e176209
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-21 20:38:24 +0900

    common에 설정 추가

--- module-common/src/main/java/com/sparta/config/SwaggerConfig.java
+++ module-common/src/main/java/com/sparta/config/SwaggerConfig.java
@@ -4,16 +4,20 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
 
+    @Value("${swagger.server.url}")  // 추가된 부분
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("/adjustment"))
+                .addServersItem(new Server().url(serverUrl))
                 .components(new Components())
                 .info(apiInfo());
     }
@@ -21,7 +25,7 @@ public class SwaggerConfig {
     private Info apiInfo() {
         return new Info()
                 .title("Swagger")
-                .description("WeatherWear Web Project API")
+                .description("Adjustment Service API")
                 .version("1.0.0");
     }
 }
--- module-gateway/src/main/java/com/sparta/config/SwaggerConfig.java
+++ module-gateway/src/main/java/com/sparta/config/SwaggerConfig.java
@@ -4,15 +4,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
-
-    @Value("${swagger.server.url}")  // 추가된 부분
-    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
