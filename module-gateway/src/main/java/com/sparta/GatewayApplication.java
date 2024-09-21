a6c90b8191995024780a31eedc3e4ce09db8fc09
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-21 11:35:15 +0900

    스프링 게이트웨이 설정

---
+++ module-gateway/src/main/java/com/sparta/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.sparta;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class GatewayApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
