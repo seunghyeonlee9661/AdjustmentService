5a032a6da532acde04ce3bf02719ec1b3e6d8637
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-22 16:58:11 +0900

    각 모듈이 api-docs가 작동하도록 설정

--- module-common/src/main/java/com/sparta/config/SwaggerConfig.java
+++ module-common/src/main/java/com/sparta/config/SwaggerConfig.java
@@ -3,11 +3,19 @@ package com.sparta.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
+
+    @Value("openapi.info.title")
+    private String title;
+    @Value("openapi.info.description")
+    private String description;
+    @Value("openapi.info.version")
+    private String version;
 
     @Bean
     public OpenAPI openAPI() {
