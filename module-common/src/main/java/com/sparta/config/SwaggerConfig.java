055cfea8f66819a5d55be76a668a6e715f2f312b
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-22 19:26:21 +0900

    Swagger base api 추가

--- module-common/src/main/java/com/sparta/config/SwaggerConfig.java
+++ module-common/src/main/java/com/sparta/config/SwaggerConfig.java
@@ -3,9 +3,12 @@ package com.sparta.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
 public class SwaggerConfig {
@@ -16,10 +19,13 @@ public class SwaggerConfig {
     private String description;
     @Value("${openapi.info.version}")
     private String version;
+    @Value("${openapi.url}")
+    private String url;
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(Collections.singletonList(new Server().url(url))) // 변경된 요청 URL
                 .components(new Components())
                 .info(apiInfo());
     }
