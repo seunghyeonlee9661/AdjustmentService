dccee45c1a87c74c8424171b805b94b28ee3eab2
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-09-22 16:31:58 +0900

    video, user 모듈에 스웨거 설정 추가

--- module-common/src/main/java/com/sparta/config/SwaggerConfig.java
+++ module-common/src/main/java/com/sparta/config/SwaggerConfig.java
@@ -3,18 +3,24 @@ package com.sparta.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
-                .info(new Info().title("Adjust Service API")
-                        .description("API documentation Adjustment Service")
-                        .version("1.0.0"));
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("API Test") // API의 제목
+                .description("Let's practice Swagger UI") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
     }
 }
